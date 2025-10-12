17ed4fe3a5432b21eb48c91de21df5a09538fed4
AminSafari207 <safarin207368@gmail.com>
2025-10-12 14:13:59 +0330

    feat(back-model): add BaseEntity entity model

---
+++ backend/src/main/java/com/fsmw/model/common/BaseEntity.java
@@ -0,0 +1,45 @@
+package com.fsmw.model.common;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@NoArgsConstructor
+@SuperBuilder(toBuilder = true)
+public abstract class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "base_seq")
+    @SequenceGenerator(name = "base_seq", sequenceName = "base_seq", allocationSize = 1)
+    @Column(name = "id", updatable = false, nullable = false)
+    private Long id;
+
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = this.createdAt;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id +
+                "\nCreatedAt: " + createdAt +
+                "\nUpdatedAt: " + updatedAt;
+    }
+}
