4af1647078b593734f916db429f7c89fe76d233d
AminSafari207 <safarin207368@gmail.com>
2025-10-12 14:58:53 +0330

    feat(back-model): add User entity model

---
+++ backend/src/main/java/com/fsmw/model/user/User.java
@@ -0,0 +1,45 @@
+package com.fsmw.model.user;
+
+import com.fsmw.model.common.BaseEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true, exclude = "movies")
+@ToString(callSuper = true, exclude = {"password", "movies"})
+@SuperBuilder(toBuilder = true)
+@Entity
+@Table(
+        name = "users",
+        uniqueConstraints = @UniqueConstraint(columnNames = "email")
+)
+public class User extends BaseEntity {
+    @NotBlank
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @NotBlank
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @NotBlank
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @ManyToMany
+    @JoinTable(
+        name = "watchlist",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "movie_id"),
+        uniqueConstraints = @UniqueConstraint(columnNames = {"user_id", "movie_id"})
+    )
+    @Builder.Default
+    private Set<Movie> movies = new HashSet<>();
+}
