bb9bb86bd2f10af8cfc701e91b6a59b4a4e107bf
AminSafari207 <safarin207368@gmail.com>
2025-10-13 21:44:54 +0330

    feat(back/service): add user service interface and impl class

---
+++ backend/src/main/java/com/fsmw/service/user/UserService.java
@@ -0,0 +1,7 @@
+package com.fsmw.service.user;
+
+import com.fsmw.model.user.User;
+import com.fsmw.service.base.BaseService;
+
+public interface UserService extends BaseService<User, Long> {
+}
---
+++ backend/src/main/java/com/fsmw/service/user/UserServiceImpl.java
@@ -0,0 +1,16 @@
+package com.fsmw.service.user;
+
+import com.fsmw.model.user.User;
+import com.fsmw.repository.user.UserRepository;
+import com.fsmw.repository.user.UserRepositoryImpl;
+import com.fsmw.service.base.AbstractBaseService;
+import com.fsmw.service.base.BaseService;
+
+public class UserServiceImpl
+        extends AbstractBaseService<User, Long, UserRepository>
+        implements BaseService<User, Long> {
+
+    public UserServiceImpl() {
+        super(UserRepositoryImpl::new);
+    }
+}
