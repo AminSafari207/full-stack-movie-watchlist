f87facf270f123c5a8db861400f23d500a9b2a32
AminSafari207 <safarin207368@gmail.com>
2025-10-13 21:47:12 +0330

    feat(back/service): add movie service interface and impl class

---
+++ backend/src/main/java/com/fsmw/service/movie/MovieService.java
@@ -0,0 +1,7 @@
+package com.fsmw.service.movie;
+
+import com.fsmw.model.movie.Movie;
+import com.fsmw.service.base.BaseService;
+
+public interface MovieService extends BaseService<Movie, Long> {
+}
---
+++ backend/src/main/java/com/fsmw/service/movie/MovieServiceImpl.java
@@ -0,0 +1,16 @@
+package com.fsmw.service.movie;
+
+import com.fsmw.model.movie.Movie;
+import com.fsmw.repository.movie.MovieRepository;
+import com.fsmw.repository.movie.MovieRepositoryImpl;
+import com.fsmw.service.base.AbstractBaseService;
+import com.fsmw.service.base.BaseService;
+
+public class MovieServiceImpl
+        extends AbstractBaseService<Movie, Long, MovieRepository>
+        implements BaseService<Movie, Long> {
+
+    public MovieServiceImpl() {
+        super(MovieRepositoryImpl::new);
+    }
+}
