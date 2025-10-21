7de731385a8177a90240c73e0075d22f3af71746
AminSafari207 <safarin207368@gmail.com>
2025-10-21 04:16:42 +0330

    feat(back/servlet): Implement login handler for auth servlet

--- backend/src/main/java/com/fsmw/servlet/AuthServlet.java
+++ backend/src/main/java/com/fsmw/servlet/AuthServlet.java
@@ -11,9 +11,11 @@ import com.fsmw.model.user.User;
 import com.fsmw.service.ServiceProvider;
 import com.fsmw.service.auth.RoleService;
 import com.fsmw.service.user.UserService;
+import com.fsmw.session.SessionManager;
 import com.fsmw.utils.PasswordUtil;
-import com.fsmw.utils.validators.ServletRequestValidator;
+import com.fsmw.utils.ServletUtil;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -45,6 +47,8 @@ public class AuthServlet extends HttpServlet {
 
         if ("/register".equals(path)) {
             handleRegister(req, resp);
+        } else if ("/login".equals(path)) {
+            handleLogin(req, resp);
         } else {
             resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
             mapper.writeValue(
@@ -61,9 +65,9 @@ public class AuthServlet extends HttpServlet {
         resp.setContentType("application/json");
 
         try {
-            String username = ServletRequestValidator.validateBlanksOrNullString(req, resp, mapper, "username");
-            String email    = ServletRequestValidator.validateBlanksOrNullString(req, resp, mapper, "email");
-            String password = ServletRequestValidator.validateBlanksOrNullString(req, resp, mapper, "password");
+            String username = ServletUtil.validateBlanksOrNullString(req, resp, mapper, "username");
+            String email    = ServletUtil.validateBlanksOrNullString(req, resp, mapper, "email");
+            String password = ServletUtil.validateBlanksOrNullString(req, resp, mapper, "password");
 
             password = PasswordUtil.encode(password);
 
@@ -118,11 +122,80 @@ public class AuthServlet extends HttpServlet {
                     ApiResponseDto.success(
                             HttpServletResponse.SC_CREATED,
                             "",
-                            "User registered successfuly",
+                            "User registered successfully",
                             userDto
                     )
             );
-        } catch (ServletRequestValidator.ValidationException ignored) {
+        } catch (ServletUtil.ValidationException ignored) {
+        } catch (Exception e) {
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+
+            try {
+                mapper.writeValue(resp.getWriter(),
+                        ApiResponseDto.error(
+                                HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+                                e.getMessage(),
+                                "Something went wrong"
+                        )
+                );
+            } catch (IOException ioException) {
+                throw new RuntimeException(ioException);
+            }
+        }
+    }
+
+    private void handleLogin(HttpServletRequest req, HttpServletResponse resp) {
+        resp.setContentType("application/json");
+
+        try {
+            String username = ServletUtil.validateBlanksOrNullString(req, resp, mapper, "username");
+            String password = ServletUtil.validateBlanksOrNullString(req, resp, mapper, "password");
+
+            Optional<User> userOpt = userService.findByUsername(username);
+
+            if (userOpt.isEmpty() || !PasswordUtil.validate(password, userOpt.get().getPassword())) {
+                resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+                mapper.writeValue(
+                        resp.getWriter(),
+                        ApiResponseDto.error(
+                                HttpServletResponse.SC_UNAUTHORIZED,
+                                "username or password not found",
+                                "Username or password is not incorrect"
+                        )
+                );
+
+                return;
+            }
+
+            User foundUser = userOpt.get();
+            String sessionId = SessionManager.createSession(foundUser.getId(), foundUser.getRoles());
+
+            Cookie cookie = new Cookie("CSESSIONID", sessionId);
+            cookie.setHttpOnly(true);
+            cookie.setSecure(false);
+            cookie.setPath("/");
+
+            resp.addCookie(cookie);
+
+            UserDto userDto = new UserDto(
+                    foundUser.getId(),
+                    foundUser.getUsername(),
+                    foundUser.getEmail()
+            );
+
+            resp.setStatus(HttpServletResponse.SC_OK);
+
+            mapper.writeValue(
+                    resp.getWriter(),
+                    ApiResponseDto.success(
+                            HttpServletResponse.SC_OK,
+                            "",
+                            "Login successful",
+                            userDto
+                    )
+            );
+        } catch (ServletUtil.ValidationException ignored) {
         } catch (Exception e) {
             resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
 
