b04b2ec453c021b459bf64b465a0558d327ffe5b
AminSafari207 <safarin207368@gmail.com>
2025-10-24 09:58:23 +0330

    refactor(front/api-service): update mainApiService to latest provided endpoints version

--- frontend/src/shared/api-services/mainApiService.js
+++ frontend/src/shared/api-services/mainApiService.js
@@ -1,22 +1,23 @@
 import { mainApiInstance } from 'src/shared/axios'
 import { endPoints } from 'src/shared/constants'
 
-const _get = async (url, ...args) => mainApiInstance.get(url, ...args)
 const _post = async (url, ...args) => mainApiInstance.post(url, ...args)
-const _delete = async (url, ...args) => mainApiInstance.delete(url, ...args)
 
-// users
-export const getUser = (...args) => _get(endPoints.servlet.users, ...args)
-export const getUsers = (...args) => _get(endPoints.servlet.users, ...args)
-export const addUser = (...args) => _post(endPoints.servlet.users, ...args)
-export const deleteUser = (...args) => _delete(endPoints.servlet.users, ...args)
-// movies
-export const getMovie = (...args) => _get(endPoints.servlet.movies, ...args)
-export const getMovies = (...args) => _get(endPoints.servlet.movies, ...args)
-export const addMovie = (...args) => _post(endPoints.servlet.movies, ...args)
-export const deleteMovie = (...args) => _delete(endPoints.servlet.movies, ...args)
+// auth
+export const signupUser = (...args) => _post(endPoints.fsmw.auth.signupUser, ...args)
+export const loginUser = (...args) => _post(endPoints.fsmw.auth.loginUser, ...args)
+
+// user
+export const getUserProfile = (...args) => _post(endPoints.fsmw.user.getUserProfile, ...args)
+export const editUserProfile = (...args) => _post(endPoints.fsmw.user.editUserProfile, ...args)
+
+// movie
+export const getMovies = (...args) => _post(endPoints.fsmw.movie.getMovies, ...args)
+export const addMovie = (...args) => _post(endPoints.fsmw.movie.addMovie, ...args)
+export const editMovie = (...args) => _post(endPoints.fsmw.movie.editMovie, ...args)
+export const deleteMovie = (...args) => _post(endPoints.fsmw.movie.deleteMovie, ...args)
+
 // watchlist
-export const getWatchlist = (...args) => _get(endPoints.servlet.watchlist, ...args)
-export const getWatchlists = (...args) => _get(endPoints.servlet.watchlist, ...args)
-export const addWatchlist = (...args) => _post(endPoints.servlet.watchlist, ...args)
-export const deleteWatchlist = (...args) => _delete(endPoints.servlet.watchlist, ...args)
+export const getWatchlist = (...args) => _post(endPoints.fsmw.watchlist.getWatchlist, ...args)
+export const addWatchlist = (...args) => _post(endPoints.fsmw.watchlist.addWatchlist, ...args)
+export const deleteWatchlist = (...args) => _post(endPoints.fsmw.watchlist.deleteWatchlist, ...args)
